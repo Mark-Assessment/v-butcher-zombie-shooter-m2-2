d9a3a73268d24ec1d6421b497d87b8bc0df6a931
veebee4 <varinia_b@hotmail.com>
2024-05-07 19:23:02 +0000

    complete timer function add alert for sound and delayafter alert bbefore start of game

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,7 @@ let timeUp = false;
 let score = 0;
 const audio = new Audio("assets/audio/shot.mp3");
 
+
 // function to get the zombies to pop up from behind the headstones at random times
 function randomTime (min, max) { // 
     return Math.round(Math.random() * (max - min) + min);
@@ -39,15 +40,32 @@ function popUp() {
     }, time);
 }
 
+// function to display a timer counting down number of seconds left - adapted from countdown tutorial on https://courses.wesbos.com/
+// and https://stackoverflow.com/questions/31106189/create-a-simple-10-second-countdown
+function timer() {
+    let timeLeft = 20;
+    const timerDisplay = document.querySelector('.display-time-left');
+    const timer = setInterval(() => {
+        if (timeLeft <= 0) {
+          clearInterval(timer);
+          timerDisplay.innerHTML = "Finished";
+        } else {
+          timerDisplay.innerHTML = `${timeLeft} second${timeLeft !== 1 ? 's' : ''} remaining`;
+        }
+        timeLeft -= 1;
+      }, 1000);
+    }
+
 // function adding a click event to start button for user to initiate the game
 function startGame() {
     scoreBoard.textContent = 0;
     timeUp = false;
     score = 0;
+    timer();
     popUp();
-    setTimeout(() => { // sets the time to be up after 10 seconds/10000ms
+    setTimeout(() => { // sets the time to be up after 20 seconds/20000ms
         timeUp = true;
-    }, 10000);
+    }, 20000);
 }
 
 // function for "shooting" the zombie when mouse is clicked (event) on zombie picture
@@ -58,22 +76,8 @@ function shoot(e) {
     scoreBoard.textContent = score;
 }
 
-// function to show a timer
-function timer(seconds) {
-    let counter = seconds;
-      
-    const interval = setInterval(() => {
-      console.log(counter);
-      counter--;
-        
-      if (counter < 0 ) {
-        clearInterval(interval);
-        console.log('Ding!');
-      }
-    }, 1000);
-  }
-
-zombies.forEach(zombie => zombie.addEventListener('click', shoot)); // event listener to look out for a click on the zombies and then run function shoot
+// event listener to look out for a click on the zombies and then run function shoot
+zombies.forEach(zombie => zombie.addEventListener('click', shoot)); 
 
 // plays a sound of a gun shot when the zombies are clicked on - obtained and adapted from tutorial - https://dev.to/shantanu_jana/how-to-play-sound-on-button-click-in-javascript-3m48
 zombies.forEach(zombie => {
