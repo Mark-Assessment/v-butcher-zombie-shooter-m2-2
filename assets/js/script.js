05909e66b9af9d820eb57ef5d8e2001fd8007c60
veebee4 <varinia_b@hotmail.com>
2024-05-19 10:19:25 +0000

    add function to change difficulty setting

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,8 @@ const audio = new Audio("assets/audio/shot.mp3");
 let lastGrave;
 let timeUp = false;
 let score = 0;
+let gameDuration = 2000; // sets default game duration to 20 seconds
+let timerInterval;
 
 
 // function to get a random time for the zombies to pop up
@@ -36,18 +38,21 @@ function startGame() {
         title: "Warning!",
         text: "This game contains loud sound! Make sure your volume is at an appropriate level",
         icon: "warning",
-        allowOutsideClick: false,
-    }).then(() => {
-    setTimeout(() => {
-    scoreBoard.textContent = 0;
-    timeUp = false;
-    score = 0;
-    timer();
-    popUp();
-    setTimeout(() => {
-        timeUp = true;
-            }, 20000);
-        }, 2000);
+        confirmButtonText: "Start Game",
+        allowOutsideClick: false
+    }).then((result) => {
+        if (result.isConfirmed) {
+            setTimeout(() => {
+                scoreBoard.textContent = 0;
+                timeUp = false;
+                score = 0;
+                timer();
+                popUp();
+                setTimeout(() => {
+                    timeUp = true;
+                }, gameDuration);
+            }, 2000);
+        }
     });
 }
 
@@ -72,11 +77,12 @@ adapted from countdown tutorial on https://courses.wesbos.com/
 and https://stackoverflow.com/questions/31106189/create-a-simple-10-second-countdown
 */
 function timer() {
-    let timeLeft = 20;
+    clearInterval(timerInterval); // Clear any previous timer intervals
+    let timeLeft = gameDuration / 1000;
     const timerDisplay = document.querySelector('.display-time-left');
-    const interval = setInterval(() => {
+    timerInterval = setInterval(() => {
         if (timeLeft <= 0) {
-            clearInterval(interval);
+            clearInterval(timerInterval);
             timerDisplay.innerHTML = "Finished";
         } else {
             const remainingSeconds = (timeLeft !== 1) ? 's' : '';
@@ -115,28 +121,45 @@ function changeDifficulty() {
     });
 }
 
-
-
-function displayDifficulty(difficulty) {
-        alert(`Difficulty set to ${difficulty}.`);
+// Function to set the game duration based on difficulty
+function difficultySetting(difficulty) {
+    switch (difficulty) {
+        case 'easy':
+            gameDuration = 30000; // 30 seconds for easy difficulty
+            break;
+        case 'normal':
+            gameDuration = 20000; // 20 seconds for normal difficulty
+            break;
+        case 'hard':
+            gameDuration = 15000; // 15 seconds for hard difficulty
+            break;
+        case 'superhard':
+            gameDuration = 10000; // 10 seconds for super hard difficulty
+            break;
+        default:
+            gameDuration = 20000; // Default duration if difficulty is not recognized
+            break;
     }
+}
 
 function gameOver() {
-    if(timeUp) {
-        if(score > 0) {
-        Swal.fire({
-            title: "Winner!",
-            text: "You got " + score + " Points!",
-            icon: "success"
-          });
-        } else {
-            Swal.fire({
-                title: "Bad Luck!",
-                text: "You didn't score any points!",
-                icon: "error"
-            });
+    setTimeout(() => {
+        if(timeUp) {
+            if(score > 0) {
+                Swal.fire({
+                    title: "Winner!",
+                    text: "You got " + score + " Points!",
+                    icon: "success"
+                });
+            } else {
+                Swal.fire({
+                    title: "Bad Luck!",
+                    text: "You didn't score any points!",
+                    icon: "error"
+                });
+            }
         }
-    }
+    }, 1000) // adds a delay after the end of the game to display the score
 }
 
 function darkMode() {
@@ -145,6 +168,27 @@ function darkMode() {
 }
 
 // Event Listeners
+
+// Event listeners for difficulty buttons
+document.getElementById('easyBtn').addEventListener('click', () => {
+    difficultySetting('easy');
+    Swal.fire("Difficulty: Easy selected!");
+});
+
+document.getElementById('normalBtn').addEventListener('click', () => {
+    difficultySetting('normal');
+    Swal.fire("Difficulty: Normal selected!");
+});
+
+document.getElementById('hardBtn').addEventListener('click', () => {
+    difficultySetting('hard');
+    Swal.fire("Difficulty: Hard selected!");
+});
+
+document.getElementById('superHardBtn').addEventListener('click', () => {
+    difficultySetting('superhard');
+    Swal.fire("Difficulty: Super Hard selected!");
+});
 
  // Event listener to look out for a click on the zombies and then run function shoot
 zombies.forEach((zombie) => {
